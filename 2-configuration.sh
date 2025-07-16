e9f698fd9f0af51753bf72fa25139f8c8e3aebed
Int3rQx <Inter23jul@gmail.com>
2025-07-16 07:51:53 +0700

    Add files via upload

--- 2-configuration.sh
+++ 2-configuration.sh
@@ -16,16 +16,6 @@ hwclock --systohc
 date
 
 # ------------------------------------------------------
-# Update reflector
-# ------------------------------------------------------
-echo "Start reflector..."
-reflector --verbose -l 200 -n 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
-# ------------------------------------------------------
-# Synchronize mirrors
-# ------------------------------------------------------
-pacman -Syy
-
-# ------------------------------------------------------
 # Install Packages
 # ------------------------------------------------------
 pacman --noconfirm -S grub xdg-desktop-portal-wlr efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call dnsmasq openbsd-netcat ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font exa bat htop ranger zip unzip neofetch duf xorg xorg-xinit xclip grub-btrfs xf86-video-amdgpu xf86-video-nouveau xf86-video-intel xf86-video-qxl brightnessctl pacman-contrib inxi
